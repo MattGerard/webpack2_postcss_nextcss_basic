ece021f3c7e06e698cc36dd999491d55df04d74e
mattgerard <matt@matthewgerard.com>
2017-03-19 06:57:59 -0700

    fix archie link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import styles from './styles/style.css';
 import Nav from './components/Nav';
-import archie from './img/archiehat.jpg';
+import archie from './img/archie_hat.jpg';
 
 const newMessage = () => (`<div>hi</div>`);
 const app = document.getElementById('app');
